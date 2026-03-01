21f388ce009edc9312613750e18cad8705734d83
Abhishek Garg <142727927+abhigarg05@users.noreply.github.com>
2026-03-01 19:20:16 +0530

    Replace Remotion with FFmpegRenderer in main

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import path from "path";
 import fs from "fs-extra";
 
 import { Kokoro } from "./short-creator/libraries/Kokoro";
-import { Remotion } from "./short-creator/libraries/Remotion";
+import { FFmpegRenderer } from "./short-creator/libraries/FFmpegRenderer";
 import { Whisper } from "./short-creator/libraries/Whisper";
 import { FFMpeg } from "./short-creator/libraries/FFmpeg";
 import { PexelsAPI } from "./short-creator/libraries/Pexels";
@@ -24,15 +24,14 @@ async function main() {
 
   const musicManager = new MusicManager(config);
   try {
-    logger.debug("checking music files");
     musicManager.ensureMusicFilesExist();
   } catch (error: unknown) {
     logger.error(error, "Missing music files");
     process.exit(1);
   }
 
-  logger.debug("initializing remotion");
-  const remotion = await Remotion.init(config);
+  logger.debug("initializing ffmpeg renderer");
+  const renderer = await FFmpegRenderer.init(config);
   logger.debug("initializing kokoro");
   const kokoro = await Kokoro.init(config.kokoroModelPrecision);
   logger.debug("initializing whisper");
@@ -44,7 +43,7 @@ async function main() {
   logger.debug("initializing the short creator");
   const shortCreator = new ShortCreator(
     config,
-    remotion,
+    renderer,
     kokoro,
     whisper,
     ffmpeg,
@@ -53,29 +52,21 @@ async function main() {
   );
 
   if (!config.runningInDocker) {
-    // the project is running with npm - we need to check if the installation is correct
     if (fs.existsSync(config.installationSuccessfulPath)) {
       logger.info("the installation is successful - starting the server");
     } else {
-      logger.info(
-        "testing if the installation was successful - this may take a while...",
-      );
+      logger.info("testing if the installation was successful - this may take a while...");
       try {
         const audioBuffer = (await kokoro.generate("hi", "af_heart")).audio;
         await ffmpeg.createMp3DataUri(audioBuffer);
         await pexelsApi.findVideo(["dog"], 2.4);
         const testVideoPath = path.join(config.tempDirPath, "test.mp4");
-        await remotion.testRender(testVideoPath);
+        await renderer.testRender(testVideoPath);
         fs.rmSync(testVideoPath, { force: true });
-        fs.writeFileSync(config.installationSuccessfulPath, "ok", {
-          encoding: "utf-8",
-        });
+        fs.writeFileSync(config.installationSuccessfulPath, "ok", { encoding: "utf-8" });
         logger.info("the installation was successful - starting the server");
       } catch (error: unknown) {
-        logger.fatal(
-          error,
-          "The environment is not set up correctly - please follow the instructions in the README.md file https://github.com/gyoridavid/short-video-maker",
-        );
+        logger.fatal(error, "Environment setup failed.");
         process.exit(1);
       }
     }
@@ -83,9 +74,7 @@ async function main() {
 
   logger.debug("initializing the server");
   const server = new Server(config, shortCreator);
-  const app = server.start();
-
-  // todo add shutdown handler
+  server.start();
 }
 
 main().catch((error: unknown) => {
